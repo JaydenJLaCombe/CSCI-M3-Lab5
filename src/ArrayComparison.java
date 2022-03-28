0db5c099db90e05f724789886d9283e3be340d4c
Jayden LaCombe <jaydenjameslacombe@gmail.com>
2022-03-28 07:06:19 -0500

    finished project with lab report turned in
    final commit
    Project Complete

--- src/ArrayComparison.java
+++ src/ArrayComparison.java
@@ -4,37 +4,66 @@
  * 3/14/2022
  */
 
-import java.util.Random;
+import java.util.*;
 
 public class ArrayComparison {
 
     public static void main(String[] arg) {
 
-        int higherLimit = 10;
-        int lowerLimit = -10;
-        int size = 15;
+        int higherLimit = 15;
+        int lowerLimit = 10;
+        int size = 10;
 
-        int[] firstArray = initializeArray(higherLimit, lowerLimit, size);
-        printArray(firstArray);
+        int[] firstArray = initializeArray (higherLimit, lowerLimit, size);
+        int[] secondArray = initializeArray (higherLimit, lowerLimit, size);
+        printTwoArrays(firstArray, secondArray);
+        compareArrays(firstArray, secondArray, size);
+
     }
 
-    public static void printArray(int[] array) {
-        for (int dex = 0; dex < array.length; dex++) {
-            String dex_str1 = String.format("index %2d:  %3d", dex, array[dex]);
-            String dex_str2 = String.format("%3d", array[dex]);
-            System.out.print(dex_str1+ " " +dex_str2 + "" + "\n");
+    public static void printTwoArrays(int[] array1, int[] array2) {
+
+        for (int dex = 0; dex < array1.length; dex++) {
+            String dex_str1 = String.format("index%3d:  %3d %3d\n", dex, array1[dex], array2[dex]);
+            System.out.print(dex_str1);
         }
+    }
+
+    public static void compareArrays(int[] array1, int[] array2, int num_elements) {
+
+        int highsArray1 = 0;
+        int highsArray2 = 0;
+        int ties = 0;
+        for (int dex = 0; dex < num_elements; dex++) {
+            if (array1[dex] > array2[dex]) {
+                highsArray1++;
+            } else if (array1[dex] < array2[dex]){
+                highsArray2++;
+            } else if (array1[dex] == array2[dex]) {
+                ties++;
+            }
+        }
+
+        String arr_str1 = "\n";
+        arr_str1 += String.format("array 1 has%3d " + "highs\n", highsArray1);
+        arr_str1 += String.format("array 2 has%3d " + "highs\n", highsArray2);
+        arr_str1 += String.format("  There are%3d " + "ties", ties);
+
+        System.out.println(arr_str1);
+
     }
 
     public static int[] initializeArray(int upperLimit, int lowerLimit, int num_elements) {
+
         int[] arrayOfInts = new int[num_elements];
         Random randGen = new Random();
         int numberValues = upperLimit - lowerLimit + 1;
+
         for (int dex = 0; dex < num_elements; dex++) {
             arrayOfInts[dex] = randGen.nextInt(numberValues) + lowerLimit;
         }
+
         return arrayOfInts;
+
     }
-
-
 }
